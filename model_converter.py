0deb922dfecc8f75d091f9be610b7d140fa11b26
rheabhutani <rb64@uw.edu>
2019-10-12 21:50:07 -0700

    Final changes

--- model_converter.py
+++ model_converter.py
@@ -1,10 +1,11 @@
-from keras.models import load_model
-import coremltools
 
-model.save('trainedModel.h5')
+import coremltools
+import keras
+import h5py
 
 output_labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-coreml_model = coremltools.converters.keras.convert('trainedModel.h5', input_names=['image'], output_names=['output'],
+model = keras.models.load_model('snakey.h5')
+coreml_model = coremltools.converters.keras.convert(model, input_names=['image'], output_names=['output'],
                                                    class_labels=output_labels, image_input_names='image')
 
 coreml_model.author = 'Shreyash Nigam'
